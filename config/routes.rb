addfcd6d938d371781cf3bd1b5de5f0ddec04b6d
Maria Kovatchka <maria.kovatchka@kellogg.northwestern.edu>
2024-02-13 15:41:45 +0000

    Stage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources "tacos"
-  # get "tacos", :controller => "tacos", :action => "index"
+  # get "tacos", :controller => "tacos", :action => "index"; look for controller tacos and method index
   resources "dice"
   # get "dice", :controller => "dice", :action => "index"
   resources "cards"
